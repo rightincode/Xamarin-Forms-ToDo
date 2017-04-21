94f55da2a7f57263057b08d79718b66adaef0304
Richard L. Taylor Jr <rtaylor@rightincode.com>
2017-04-21 12:19:18 -0400

    Added new azure mobile app

---
+++ ToDo.MobileAppService/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ToDo.MobileAppService.Startup))]
+
+namespace ToDo.MobileAppService
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
