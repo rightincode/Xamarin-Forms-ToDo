8f8f4dd20dede18af8d6c861932ad94cc13575b7
Richard L. Taylor Jr <rtaylor@rightincode.com>
2017-04-21 10:05:26 -0400

    Added windows UWP target to solution

---
+++ ToDo.UWP/FileHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Windows.Storage;
+
+using Xamarin.Forms;
+using ToDo.UWP;
+using ToDoPCL.Data;
+
+
+[assembly: Dependency(typeof(FileHelper))]
+namespace ToDo.UWP
+{
+    public class FileHelper : IFileHelper
+    {
+        public string GetLocalFilePath(string filename)
+        {
+            return Path.Combine(ApplicationData.Current.LocalFolder.Path, filename);
+        }
+    }
+}
