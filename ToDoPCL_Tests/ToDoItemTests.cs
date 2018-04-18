fa1ab7caece1da6eb1d1a6a2128589db58416313
Richard L. Taylor Jr <rtaylor@rightincode.com>
2018-04-17 20:35:32 -0400

    Added unit test project for ToDoPCL and tests for ToDoItem type

---
+++ ToDoPCL_Tests/ToDoItemTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoPCL.Models;
+
+namespace ToDoPCL_Tests
+{
+    [TestClass]
+    public class ToDoItemTests
+    {
+        [TestMethod]
+        public void TestSetToDoItemId()
+        {
+            var toDoItem = new ToDoItem();
+
+            toDoItem.SetToDoItemId();
+
+            Assert.IsInstanceOfType(toDoItem.Id, typeof(System.String));
+
+            //Assert.IsNotNull(toDoItem.Id);
+            //Assert.IsTrue(toDoItem.Id.Length > 0);            
+        }
+    }
+}
