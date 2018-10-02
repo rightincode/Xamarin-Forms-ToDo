8d6a6e8a51f74b14f7d442affdd28dbc9bd875eb
Richard L. Taylor Jr <rtaylor@rightincode.com>
2018-10-02 19:53:49 -0400

    Added new .net standard project (ToDo.CoreNS), added interfaces and models, updated ToDo.Cor_Tests to use new .net standard project

---
+++ ToDo.CoreNS/Models/ToDoItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
+using ToDo.Core.Interfaces;
+
+namespace ToDo.Core.Models
+{
+    public class ToDoItem : IToDoItem
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        [JsonProperty(PropertyName = "taskName")]
+        public string TaskName { get; set; }
+        [JsonProperty(PropertyName = "priority")]
+        public string Priority { get; set; }
+        [JsonProperty(PropertyName = "dueDate")]
+        public DateTime DueDate { get; set; }
+
+        //azure specific attributes
+        [JsonProperty(PropertyName = "createdAt")]
+        public DateTimeOffset CreatedAt { get; set; }
+
+        [UpdatedAt]
+        public DateTimeOffset UpdatedAt { get; set; }
+
+        [Version]
+        public string AzureVersion { get; set; }
+        
+        public ToDoItem()
+        {
+            SetToDoItemId();
+        }
+        
+        private void SetToDoItemId()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
