0428dfc2050d4890dbb740d48d8b9b8fbe57ff3b
Richard L. Taylor Jr <rtaylor@rightincode.com>
2018-10-02 20:53:24 -0400

    Created new .net standard test project (ToDo.UnitTests) and moved all of the tests from ToDoPCL_UnitTest project

---
+++ ToDo.UnitTests/CreatePageViewModelTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading.Tasks;
+using ToDo.Core.Models;
+using ToDo.ViewModels;
+
+namespace ToDo.UnitTests
+{
+    [TestClass]
+    public class CreatePageViewModelTests
+    {
+        [TestMethod]
+        public async Task AddToDoItemToDBAndRetrieveToDoItemFromDB_NewToDoItem_NewToDoItemAndVMToDoItemHaveSameProperties()
+        {
+            var testToDoItem = new ToDoItem
+            { 
+                TaskName = "Testing_AddToDoItem",
+                DueDate = new DateTime(1970, 04, 27, 4, 30, 0),
+                Priority = "Low"
+            };
+
+            var testDataStore = new MockToDoItemDatabase();
+            var vm = new CreatePageViewModel(testToDoItem, testDataStore);
+
+            await vm.AddToDoItem();
+            await vm.LoadToDoListItem(testToDoItem.Id);
+
+            Assert.AreEqual(testToDoItem.Id, vm.Id);
+            Assert.AreEqual(testToDoItem.DueDate, vm.DueDate);
+            Assert.AreEqual(testToDoItem.Priority, vm.Priority);
+        }
+
+        [TestMethod]
+        public async Task AddToDoItemToDBAndDeleteToDoItemFromDB_NewToDoItem_DeletedToDoItem()
+        {
+            var testToDoItem = new ToDoItem
+            {
+                TaskName = "Testing_DeleteToDoItem",
+                DueDate = new DateTime(1970, 04, 27, 4, 30, 0),
+                Priority = "Low"
+            };
+
+            var testDataStore = new MockToDoItemDatabase();
+            var vm = new CreatePageViewModel(testToDoItem, testDataStore);
+
+            await vm.AddToDoItem();
+            var result = await vm.DeleteToDoItem();
+
+            Assert.AreEqual(result, true);
+        }
+        
+    }
+}
---
+++ ToDo.UnitTests/ListTasksPageViewModelTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ToDo.ViewModels;
+
+namespace ToDo.UnitTests
+{
+    [TestClass]
+    public class ListTasksPageViewModelTests
+    {
+        [TestMethod]
+        public async Task RetrieveListOfTasksFromDB_OneTaskInDB_ListWithOneTask()
+        {
+            var vm = new ListTasksPageViewModel(new MockToDoItemDatabase());
+
+            var recordCount = await vm.LoadItemsAsync(true);
+
+            Assert.AreEqual(recordCount, 1);
+        }
+    }
+}
